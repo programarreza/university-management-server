d2676ebdeaef40ff8bbe293d8ceb92d0646fa8b2
programarreza <programarreza@gmail.com>
2024-06-19 21:20:11 +0600

    added enrolledCourse interface, model & validation

---
+++ src/app/modules/enrolledCourse/enrolledCourse.validation.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+const createEnrolledCourseValidationZodSchema = z.object({
+  body: z.object({
+    offeredCourse: z.string(),
+  }),
+});
+
+export = { createEnrolledCourseValidationZodSchema };
